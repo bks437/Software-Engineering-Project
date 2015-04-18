901341a0aebf716e37c2b3b5deaf6859e709181f
pkbcn6 <pkbcn6@mail.missouri.edu>
2015-04-18 12:41:01 -0500

    Moving js
    Moved js to it's external sheet

--- js/courses.js
+++ js/courses.js
@@ -1,22 +1,19 @@
-$(document).ready(function () {
-    $("#type").change(function() {
-        if ($(this).val() == "graduate") {
-            $("#years").hide();
-            $("#degrees").hide();
-            $("#programs").show();
-            $("#advisors").show();
-		}
-		else if ($(this).val() == "undergraduate")  {
-			$("#years").show();
-            $("#degrees").show();
-            $("#programs").hide();
-            $("#advisors").hide();
-		}
-		else {
-			$("#years").hide();
-            $("#degrees").hide();
-            $("#programs").hide();
-            $("#advisors").hide();
-		} 
-	}); 
-});
+function addclass(course,grade,action){
+	var xmlHttp = xmlHttpObjCreate();
+	if(!xmlHttp){
+		alert("This browser doesn't support this action");
+		return
+	}
+
+	xmlHttp.onload = function(){
+	var response = xmlHttp.responseText;
+	var isnert = document.getElementById('selected');
+		console.dir(response);
+		document.getElementById('selected').innerHTML = JSON.parse(response);
+	}
+	document.getElementById('selected').innerHTML = 'adding...';
+	var reqURL = "addcourse.php?action="+action+"&username=<? echo $_SESSION[username] ?>&course="+course+"&grade="+grade;
+	xmlHttp.open("GET", reqURL, true);
+	xmlHttp.send();
+
+}
