d74b04673cf36b433c9c975a9172431d4cbaa935
JET005 <ethanwtsn.watson5@gmail.com>
2015-04-19 12:13:19 -0500

    Finshed inserting courses.

--- phpAPP/addcourse.php
+++ phpAPP/addcourse.php
@@ -1,4 +1,6 @@
 <?php
+
+	session_start();
 		//connect to database
 	include("test/database.php");
 	//if cannot connect return error
@@ -7,15 +9,15 @@
 	$result=false;
 	if(strcmp($_GET[action],"Taught")==0){
 		pg_prepare($dbconn,"Taught",'INSERT INTO DDL.has_taught VALUES ($1,$2)');
-		$result = pg_execute($dbconn,"Taught",array($_GET[username],$_GET[course]));
+		$result = pg_execute($dbconn,"Taught",array($_SESSION[username],$_GET[course]));
 	}
 	elseif(strcmp($_GET[action],"Teaching")==0){
 		pg_prepare($dbconn,"Teaching",'INSERT INTO DDL.are_teaching VALUES ($1,$2)');
-		$result = pg_execute($dbconn,"Teaching",array($_GET[username],$_GET[course]));
+		$result = pg_execute($dbconn,"Teaching",array($_SESSION[username],$_GET[course]));
 	}
 	elseif(strcmp($_GET[action],"Wants")==0){
 		pg_prepare($dbconn,"Wants",'INSERT INTO DDL.wants_to_teach VALUES ($1,$2,$3)');
-		$result = pg_execute($dbconn,"Wants",array($_GET[username], $_GET[grade],$_GET[course]));
+		$result = pg_execute($dbconn,"Wants",array($_SESSION[username], $_GET[grade],$_GET[course]));
 	}
 	if(!$result)
 		echo json_encode("Failed to add class.");
