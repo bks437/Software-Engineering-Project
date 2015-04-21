8b7852cc5fd245f04f2aa21773cd5a1dca4a3dc0
wangso <wangso@missouri.edu>
2015-04-21 09:11:54 -0500

    update professor page

--- phpAPP/professor_page.php
+++ phpAPP/professor_page.php
@@ -43,14 +43,12 @@
 				$student = $row['ta_username'];
 				echo "<tr>";
 					echo "<td>$student</td>";
-					echo "<td><a href=\"#\">Student Info</td>";
-					echo "<td><a href=\"#\">Add Comments</td>";
-					echo "<td><a href=\"#\">View Resume</td>";
-					echo "<td><a href=\"#\">Request as TA</td>";
-
+					echo "<td><a href=\"stu_info.php?ta_username=Value\">Student Info</a></td>";
+					echo "<td><a href=\"add_comment.php?ta_username=Value\">Add Comments</a></td>";
+					echo "<td><a href=\"view_resume.php?ta_username=Value\">View Resume</a></td>";
+					echo "<td><a href=\"request_as_ta.php?ta_username=Value\">Request as TA</a></td>";
 				echo "</tr>";	
-
-			$i++; 								
+			$i++; 									
 			}	
 		}
 
@@ -58,7 +56,7 @@
 
 
 	//if search by applicant name
-	elseif (isset($_POST['ASearch'])) {
+	if (isset($_POST['ASearch'])) {
 
 		//connect to database
 		include("../phpSQL/test/database.php");
@@ -66,48 +64,87 @@
 		$dbconn=pg_connect(HOST." ".DBNAME." ".USERNAME." ".PASSWORD)
 				or die('Could not connect: ' . pg_last_error());
 
-		//find applicant userID based on provided name;
-		$applicantName = $_POST['applicantName'];
+		//find applicant userID based on provided first name and/or last name;
+		if($_POST['applicant_fName']) {
 
-		// need to work this out using a full name
-		$username_find = "SELECT P.username from DLL.person P where P.fname = '$applicantName' AND P.lname = '$applicantName';"; 
-		$result= pg_query($dbconn, $username_find)or die('error! ' . pg_last_error());
+			//if both first name and last name are searched
+			if($_POST['applicant_lName']) {
 
-		//find courseIDs wanted by the TA searched for;
-		$query = "SELECT wtt.c_id FROM DLL.wants_to_teach wtt  where wtt.ta_username = '$result' ORDER by c_id;";
-		$result= pg_query($dbconn, $query)or die('error! ' . pg_last_error());
-		
+				$applicant_fName = $_POST['applicant_fName'];
+				$applicant_lName = $_POST['applicant_lName'];
+				$username_find = "SELECT P.username from DDL.person P where lower(P.fname) = lower('$applicant_fName') AND lower(P.lname) = lower('$applicant_lName');";
+				$result= pg_query($dbconn, $username_find)or die('error! ' . pg_last_error());
+			
+			}
+
+			//if only first name is searched
+			else {
+
+				$applicant_fName = $_POST['applicant_fName'];
+				$username_find = "SELECT P.username from DDL.person P where lower(P.fname) = lower('$applicant_fName');";
+				$result= pg_query($dbconn, $username_find)or die('error! ' . pg_last_error());
+			}
+		}
+
+		//if only last name is searched
+		elseif ($_POST['applicant_lName']){
+				$applicant_lName = $_POST['applicant_lName'];
+				$username_find = "SELECT P.username from DDL.person P where lower(P.lname) = lower('$applicant_lName');";
+				$result= pg_query($dbconn, $username_find)or die('error! ' . pg_last_error());
+				
+		}	
+		else{
+			$result = false;
+			echo "Wrong! Both first name and last name can not be empty!".'<br/><br/><br/>';
+		}
+
+
 		if ($result == false) {
 			$_SESSION['form_search']=false;
-		}
+		}	
 		else {
 
-			//display result in tables with link to course info;
-			echo "<table border ='1'>";
-				echo "<tr>";
-					echo "<td><b>Course ID</b></td>";
-					echo "<td><b>Course Info</b></td>";
+			if(pg_num_rows($result)!=0) {
 
-				echo "</tr>";
+				$search_result = pg_fetch_array($result)[0];
+					echo "username is:".$search_result.'<br/>';
+					echo "arrar size is:".(pg_num_rows($result)).'<br/>';
 
-			$i=1;
-			while($row = pg_fetch_array($result)) {
-				$course = $row['c_id'];
-				echo "<tr>";
-					echo "<td>$course</td>";
-					echo "<td><a href=\"#\">Course Info</td>";
-				echo "</tr>";	
+				//find courseIDs wanted by the TA searched for;
+				$query = "SELECT wtt.c_id FROM DDL.wants_to_teach wtt  where wtt.ta_username = '$search_result' ORDER by c_id;";
+				$result= pg_query($dbconn, $query)or die('error! ' . pg_last_error());
+		
+				//display result in tables with link to course info;
+				echo "<table border ='1'>";
+					echo "<tr>";
+						echo "<td>Course ID</td>";
+						echo "<td>Course Info</td>";
 
-			$i++; 								
-			}	
-		}
+					echo "</tr>";
+
+				$num_rows = pg_num_rows($result);
+				echo "number of rows:".$num_rows;
+				$i=1;
+				while($row = pg_fetch_array($result)) {
+					$course = $row['c_id'];
+					echo "<tr>";
+						echo "<td>$course</td>";
+						echo "<td><a href=\"#\">Course Info</td>";
+					echo "</tr>";	
+				}
+			}		
+
+			else{
+				echo "No matching data found!";
+			}
+		}	
+
 	}
-	
-
-//	else {
-//		header("Location: professor_page.php");
-//		pg_close($dbconn);	
-//	}
+		
+	//go to home page
+	if (isset($_POST['homepage'])) {
+		header("Location: ../phpSQL/homepage.php");
+	}
 
 ?>
 
@@ -130,13 +167,23 @@
 				<button type="submit" name="CSearch" value="search by course">Course Search</button>
 		</div>
 		<div>
-				<label for="applicantName" > Enter the course you would like to see TA applicants: </label>
-				<input type="text" name="applicantName" id="applicantSearch" placeholder = "John Doe"></input><br>
+				<label for="applicantName" > Enter the name of applicant you would like to search: </label>
+				<input type="text" name="applicant_fName" id="applicantSearch" placeholder = "John"></input><br>
+				<input type="text" name="applicant_lName" id="applicantSearch" placeholder = "Doe"></input><br>
 				<button type="submit" name="ASearch" value="search by applicant">Applicant Search</button>
 		</div>
-		
-		
 		<!--comment on applicants of their choosing
 					button in generated table to add to comment of person-->
+
+		 <!-- Go to homepage -->
+
+		<div >
+				<label for="homepage">Go to home page</label><br>
+
+				<input type="submit" name="homepage" value="Go to home page"></input>
+		
+		</div>	
+
+
 	</body>
 </html>
