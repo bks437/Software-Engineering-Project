68c88876504a6466350aa1d39f259b42b79cc871
bks437 <bks437@mail.missouri.edu>
2015-04-27 23:40:25 -0500

    Used course.js to link remove buttons

--- js/courses.js
+++ js/courses.js
@@ -22,3 +22,27 @@ function addclass(course,action){
 	    xmlHttp.send();
 
 	}
+function removeclass(course,action){
+		if(action=="WantsRemove"){
+			var e = document.getElementById(course);
+			var grade = e.options[e.selectedIndex].value;
+			console.dir(grade);
+		}
+		var xmlHttp = xmlHttpObjCreate();
+		if(!xmlHttp){
+			alert("This browser doesn't support this action");
+			return
+		}
+
+		xmlHttp.onload = function(){
+			var response = xmlHttp.responseText;
+			var isnert = document.getElementById('selected');
+			console.dir(response);
+			document.getElementById('selected').innerHTML = JSON.parse(response);
+		}
+		document.getElementById('selected').innerHTML = 'removing...';
+		var reqURL = "removecourse.php?action="+action+"&course="+course+"&grade="+grade;
+	    xmlHttp.open("GET", reqURL, true);
+	    xmlHttp.send();
+
+	}
