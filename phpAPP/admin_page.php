5c7868965231420b776474c5ebf0f9be87390acc
wangso <wangso@missouri.edu>
2015-05-01 17:40:15 -0500

    updated a link

--- phpAPP/admin_page.php
+++ phpAPP/admin_page.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
 	if(!isset($_SESSION['username']) || $_SESSION["authority"] != "admin"){
-		header("Location: ../../index.php");
+		header("Location: ../index.php");
 	}	
 ?>
 
