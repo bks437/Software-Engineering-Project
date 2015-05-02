39d6b86894caf643fcbb6499891358f2712593e4
JET005 <ethanwtsn.watson5@gmail.com>
2015-05-01 19:43:31 -0500

    fixed location error

--- phpAPP/assign.php
+++ phpAPP/assign.php
@@ -1,7 +1,7 @@
 <?php
 
 
-include("test/database.php");
+include("../connect/database.php");
 		//if cannot connect return error
 		$dbconn=pg_connect(HOST." ".DBNAME." ".USERNAME." ".PASSWORD) or die('Could not connect: ' . pg_last_error());
 		if(strcmp($_GET[action],"add")==0){
