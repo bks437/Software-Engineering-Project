154725495bb6c50ccf0b5c25b5ee2a9178736a83
pkbcn6 <pkbcn6@mail.missouri.edu>
2015-05-02 18:12:50 -0500

    Updated courses.php and style.css
    Changed colors to not rape our eyes (dulled down)

--- Application/courses.php
+++ Application/courses.php
@@ -3,7 +3,7 @@
 	//Redirect if user is not logged in to login page
 	if(!isset($_SESSION['username']) || $_SESSION["authority"] != "applicant"){
 		header("Location: ../index.php");
-	}
+	}	
 	//connect to database
 	include("../connect/database.php");
 			//include("test/database.php");
@@ -17,18 +17,18 @@
 	 if(isset($_POST['submit'])){
 	 	if(strcmp($_SESSION['grad'],"ta")==0){
 	 		pg_prepare($dbconn, 'grad', 'INSERT INTO DDL.is_a_grad values($1,$2,$3)');
-	 		$result = pg_execute($dbconn, 'grad', array($_SESSION['username'],$_POST[gradpro],$_POST[advisor]));
+	 		$result = pg_execute($dbconn, 'grad', array($_SESSION['username'],$_POST[gradpro],$_POST[advisor])); 
 	 	}
 	 	elseif(strcmp($_SESSION['grad'],"pla")==0){
 	 		pg_prepare($dbconn, 'ungrad', 'INSERT INTO DDL.is_an_undergrad values($1,$2,$3)') or die('Could not connect: ' . pg_last_error());;
-	 		$result = pg_execute($dbconn, 'ungrad', array($_SESSION['username'],$_POST[program],$_POST[year]))or die('Could not connect: ' . pg_last_error());;
+	 		$result = pg_execute($dbconn, 'ungrad', array($_SESSION['username'],$_POST[program],$_POST[year]))or die('Could not connect: ' . pg_last_error());; 
 	 	}
 	 	if($result==false){
 	 		$_SESSION[insert]=false;
 	 	}
 	 	else
 	 		header("Location: courses.php");
-
+	
 	}*/
 ?>
 
@@ -37,36 +37,25 @@
 	<!--ADD ANY USEFUL TIPS, otherwise ... DO NOT FUCK WITH THE COMMENTS. please and thank you.-->
 <head>
 	<title>CS4320 - Group G</title>
-	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<link rel="stylesheet" type="text/css" href="../css/style.css">	
 	<script src="../js/jquery-1.11.2.min.js"></script>
 	<script type="text/javascript" src="../js/courses.js"></script>
 	<script src="../js/ajax.js"></script>
-	<style>
-	.added{
-		background: green;
-	}
-	.adding{
-		background: yellow;
-	}
-	.remove{
-		background: red;
-	}
-	</style>
 </head>
 <body>
-
+	
 	<!-- Header/Footer -->
-
-		<div class="header shadowheader">
-			<h1>Step 4: Courses</h1>
-		</div>
-
-		<div class="footer shadowfooter">
-			<h4>Copyright &copy; Group G - Computer Science Department</h4>
-		</div>
-
+		
+		<div class="header shadowheader">			
+			<h1>Step 4: Courses</h1>		
+		</div>			
+		
+		<div class="footer shadowfooter">			
+			<h4>Copyright &copy; Group G - Computer Science Department</h4>		
+		</div>		
+	
 	<!-- Courses -->
-
+		
 		<div class="centerpls">
 				<p class="centerdisplay">
 					<label class="leftlabel" for="courseteaching" >Course(s) You Are Currently Teaching: </label>
@@ -83,7 +72,7 @@
 						<option value="cs4380">CS4380</option>
 						<option value="cs4610">CS4610</option>
 						<option value="cs4830">CS4830</option>
-					</select> -->
+					</select> -->		
 					<? $query = 'SELECT c_id,name,numb FROM DDL.Course;';
 
 					$result = pg_query($query) or die('Query failed: '. pg_last_error());
@@ -104,8 +93,8 @@
 					while($line=pg_fetch_array($result,null, PGSQL_ASSOC)){
 						//foreach ($line as $col_value){
 								echo "<div id=\"Teaching".$line[c_id]."\">";
-								echo "\t\t<div class=\"name\">$line[name]</div>\n";
-								echo "\t\t<div class=\"numb\">$line[numb]</div>\n";
+								echo "\t\t<div class=\"name\">$line[name]</div>\n";	
+								echo "\t\t<div class=\"numb\">$line[numb]</div>\n";		
 							//}
 								echo "<button class=\"button\" onclick=\"addcourse('$line[c_id]','Teaching')\">Add</button>";
 								echo "<button class=\"button\" onclick=\"removecourse('$line[c_id]','Teaching')\">Remove</button></div>";
@@ -113,7 +102,7 @@
 						}
 						//free the result
 					pg_free_result($result);
-					?>
+					?>		
 					<br>
 				</p>
 				<p class="centerdisplay">
@@ -152,8 +141,8 @@
 					while($line=pg_fetch_array($result,null, PGSQL_ASSOC)){
 						//foreach ($line as $col_value){
 								echo "<div id=\"Taught".$line[c_id]."\">";
-								echo "\t\t<div class=\"name\">$line[name]</div>\n";
-								echo "\t\t<div class=\"numb\">$line[numb]</div>\n";
+								echo "\t\t<div class=\"name\">$line[name]</div>\n";	
+								echo "\t\t<div class=\"numb\">$line[numb]</div>\n";		
 							//}
 								echo "<button class=\"button\" onclick=\"addcourse('$line[c_id]','Taught')\">Add</button>";
 								echo "<button class=\"button\" onclick=\"removecourse('$line[c_id]','Taught')\">Remove</button></div>";
@@ -202,8 +191,8 @@
 					while($line=pg_fetch_array($result,null, PGSQL_ASSOC)){
 						//foreach ($line as $col_value){
 								echo "<div id=\"Wants".$line[c_id]."\">";
-								echo "\t\t<div class=\"name\">$line[name]</div>\n";
-								echo "\t\t<div class=\"numb\">$line[numb]</div>\n";
+								echo "\t\t<div class=\"name\">$line[name]</div>\n";	
+								echo "\t\t<div class=\"numb\">$line[numb]</div>\n";		
 							//}
 								echo "<select multiple class=\"niceinput\" id=$line[c_id] name=\"lgrade\">
 									<option value=\"A\" selected>A</option>
@@ -221,17 +210,17 @@
 					?>
 					<br>
 				<!--</p>-->
-			</div>
-
+			</div>	
+	
 	<!-- Next Page -->
 
 		<div class="centerpls">
 			<br><br>
 			<p class="centerdisplay nextbutton" id="click">
-				<input  type="submit" name="submit" value="Finish Application" onclick="window.location.href ='../phpSQL/home.php'">
-			</p>
+				<input  type="submit" name="submit" value="Proceed to the next step">
+			</p>			
 		</div>
-
+	
 	</body>
 </html>
 <? pg_close($dbconn);?>
