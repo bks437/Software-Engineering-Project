ce05d24d224e8411019da67a7d9807b80adf2609
Thenry945 <travis@henrichs.cc>
2015-05-02 16:46:38 -0500

    More fixes in Application/courses.php and Application/registration.php

--- Application/registration.php
+++ Application/registration.php
@@ -1,4 +1,4 @@
-<?PHP 
+<?PHP
 	session_start();
 
 	if(!isset($_SERVER['HTTPS']) || !$_SERVER['HTTPS']){
@@ -21,14 +21,14 @@
 		$password_hash = sha1($pass);
 		$action = "register";
 		//inserting username into user_info
-		
-					
-			
+
+
+
 			//if($result){
 			//inserting username, password_hash, and salt into authentication table
 			$result = pg_prepare($dbconn, "insert", 'INSERT INTO DDL.Login(username, password_hash, salt) VALUES ($1,$2,$3)') or die(pg_last_error());
 			$result = pg_execute($dbconn, "insert", array($username, $password_hash, $salt)) or die("Username alread taken ". pg_last_error());
-			
+
 			$result = pg_prepare($dbconn, "register", "INSERT INTO DDL.Person (username,fname,lname) VALUES ($1,$2,$3)");
 			$result = pg_execute($dbconn, "register", array($username,$_POST['fname'],$_POST['lname'])) or die (pg_last_error());
 
@@ -45,7 +45,7 @@
 				}
 				else{
 					echo "Please try again3";
-				}	
+				}
 			}
 			else{
 				echo "Please try again2";
@@ -54,41 +54,41 @@
 		// else{
 		// 	echo"Please try again1";
 		// }
-	pg_close($dbconn);	
-	}	
-?>	
+	pg_close($dbconn);
+	}
+?>
 <html>
-<head>		
+<head>
 	<title>CS4320 - Group G</title>
-	<link rel="stylesheet" type="text/css" href="../css/style.css">	
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
 	<script src="../js/jquery-1.11.2.min.js"></script>
 	<script type="text/javascript" src="../js/courses.js"></script>
 	<script src="../js/ajax.js"></script>
 </head>
 <body>
 		<!-- Header/Footer -->
-			
-			<div class="header shadowheader">			
-				<h1>Please Register</h1>		
-			</div>			
-			
-			<div class="footer shadowfooter">			
-				<h4>Copyright &copy; Group G - Computer Science Department</h4>		
-			</div>	
-	<form name="RegistrationProcess" action= "registration.php" method="POST">	
+
+			<div class="header shadowheader">
+				<h1>Please Register</h1>
+			</div>
+
+			<div class="footer shadowfooter">
+				<h4>Copyright &copy; Group G - Computer Science Department</h4>
+			</div>
+	<form name="RegistrationProcess" action= "registration.php" method="POST">
 		<div class="centerplsreg">
 			<div id="login"> <!-- do we need this id?? -->
-				<p>				
+				<p>
 					<label class="floatleft" for="fname">First name:</label>
 					<input class="floatright" type="text" name="fname">
 				</p>
 					<br>
 				<p>
 					<label class="floatleft" for="lname">Last name: </label>
-					<input class="floatright" type="text" name="lname">							
-				</p>		
+					<input class="floatright" type="text" name="lname">
+				</p>
 					<br>
-				<p>				
+				<p>
 					<label class="floatleft" for="username">Username:</label>
 					<input class="floatright" type="text" name ="username">
 				</p>
@@ -96,7 +96,7 @@
 				<p>
 					<label class="floatleft" for="password">Password:</label>
 					<input class="floatright" type="password" name="password">
-				</p>		
+				</p>
 			</div>
 		</div>
 			<br>
@@ -104,8 +104,8 @@
 			<br>
 		<div style="text-align: center">
 			<input type="submit" name="submit" value="Register">
-			<input type="button" name="cancel" value="Click here to return to Login page" onclick="window.location.href ='index.php'">
-		</div>				
-	</form>	
+			<input type="button" name="cancel" value="Click here to return to Login page" onclick="window.location.href ='../index.php'">
+		</div>
+	</form>
 </body>
 </html>
