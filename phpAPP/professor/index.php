b05e51a3177984f35723bd6ee38acc4db0f4f2db
pkbcn6 <pkbcn6@mail.missouri.edu>
2015-05-03 14:12:56 -0500

    Fixes 14/?
    Updating & formatting professor page

--- phpAPP/professor/index.php
+++ phpAPP/professor/index.php
@@ -2,34 +2,53 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Professor Page</title>
+	<title>CS4320 - Group G</title>
+	<link rel="stylesheet" type="text/css" href="../../css/style.css">
+	<script src="../../js/jquery-1.11.2.min.js"></script>
 </head>
 <body>
+	<form method="POST" action="professorActions/search_result.php">
+	
+	<!-- Header/Footer -->
 
-	<form method="POST" action="search_result.php">
+		<div class="header shadowheader">
+			<h1>Professor Page</h1>
+		</div>
+
+		<div class="footer shadowfooter">
+			<h4>Copyright &copy; Group G - Computer Science Department</h4>
+		</div>
+	
+	<!-- Home/Logout -->
+	
+		<div class="centerlogout">
+			<br>
+			<!--<input class="home" type="submit" name="submit" value="Home" onclick="window.location.href ='../phpSQL/index.php'">-->
+			<input class="logout" type="submit" name="submit" value="Logout" onclick="window.location.href ='../../phpSQL/logout.php'">
+			<br>
+		</div>
+	
 		<!--request TA for course
 				button in generated table to enter entry into request table-->
 		<!--view all TA applicants base on selected parameters
 				code below-->
-		<div>
-				<label for="courseNumb" > Enter the course number you would like to see TA applicants: </label>
-				<input type="text" name="courseNumb" id="courseSearch" placeholder = "CS1050"></input><br>
-				<button type="submit" name="CSearch" value="search by course">Course Search</button><br><br>
-		</div>
-		<div>
-				<label for="applicantName" > Enter the name of applicant you would like to search: </label>
-				<input type="text" name="applicant_fName" id="applicantSearch" placeholder = "first name"></input>
-				<input type="text" name="applicant_lName" id="applicantSearch" placeholder = "last name"></input><br>
-				<button type="submit" name="ASearch" value="search by applicant">Applicant Search</button><br><br>
-		</div>
-		<!--comment on applicants of their choosing
-					button in generated table to add to comment of person-->
-
-		 <!-- Go to homepage -->
-		<div >
-				<label for="homepage">Go to home page</label><br>
-				<input type="submit" name="homepage" value="Go to home page"><br><br></input>
-		</div>
+				
+		<div class="centerplsprof">
+			<label class="floatleft" for="courseNumb">View TA/PLA applicants by course number:</label>
+			<div class="floatright">
+				<input type="text" name="courseNumb" id="courseSearch" placeholder="CS1050"></input>
+				<button type="submit" name="CSearch" value="search by course">Search</button>
+					<br><br>
+			</div>
+			
+			<label class="floatleft" for="applicantName">View applicant by name: </label>
+			<div class="floatright">
+				<input type="text" name="applicant_fName" id="applicantSearch" placeholder="First Name"></input>
+				<input type="text" name="applicant_lName" id="applicantSearch" placeholder="Last Name"></input>
+				<button type="submit" name="ASearch" value="search by applicant">Search</button>
+					<br><br>
+			</div>
+		</div>		
 	</form>
-	</body>
+</body>
 </html>
