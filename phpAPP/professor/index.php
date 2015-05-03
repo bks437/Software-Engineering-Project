5a9a72ec9d9d9e249bb1a187e81a659d9261f013
Thenry945 <travis@henrichs.cc>
2015-05-03 14:42:56 -0500

    fixed logout of professor

--- phpAPP/professor/index.php
+++ phpAPP/professor/index.php
@@ -8,7 +8,7 @@
 </head>
 <body>
 	<form method="POST" action="professorActions/search_result.php">
-	
+
 	<!-- Header/Footer -->
 
 		<div class="header shadowheader">
@@ -18,21 +18,21 @@
 		<div class="footer shadowfooter">
 			<h4>Copyright &copy; Group G - Computer Science Department</h4>
 		</div>
-	
+
 	<!-- Home/Logout -->
-	
+
 		<div class="centerlogout">
 			<br>
 			<!--<input class="home" type="submit" name="submit" value="Home" onclick="window.location.href ='../phpSQL/index.php'">-->
-			<input class="logout" type="submit" name="submit" value="Logout" onclick="window.location.href ='../../phpSQL/logout.php'">
+			<button class="logout" type="submit" name="submit" value="Logout" onclick="window.location.href ='../../phpSQL/logout.php'"> </button>
 			<br><br>
 		</div>
-	
+
 		<!--request TA for course
 				button in generated table to enter entry into request table-->
 		<!--view all TA applicants base on selected parameters
 				code below-->
-				
+
 		<div class="centerplsprof">
 			<label class="floatleft" for="courseNumb">View TA/PLA applicants by course number:</label>
 			<div class="floatright">
@@ -48,7 +48,7 @@
 				<button type="submit" name="ASearch" value="search by applicant">Search</button>
 					<br><br>
 			</div>
-		</div>		
+		</div>
 	</form>
 </body>
 </html>
