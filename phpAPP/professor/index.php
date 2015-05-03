c676741aea2532a75592dbe3b21e84201beb0600
pkbcn6 <pkbcn6@mail.missouri.edu>
2015-05-03 14:22:54 -0500

    Fixes 14.5/?
    <br> tags

--- phpAPP/professor/index.php
+++ phpAPP/professor/index.php
@@ -25,7 +25,7 @@
 			<br>
 			<!--<input class="home" type="submit" name="submit" value="Home" onclick="window.location.href ='../phpSQL/index.php'">-->
 			<input class="logout" type="submit" name="submit" value="Logout" onclick="window.location.href ='../../phpSQL/logout.php'">
-			<br>
+			<br><br>
 		</div>
 	
 		<!--request TA for course
@@ -40,7 +40,7 @@
 				<button type="submit" name="CSearch" value="search by course">Search</button>
 					<br><br>
 			</div>
-			
+				<br><br>
 			<label class="floatleft" for="applicantName">View applicant by name: </label>
 			<div class="floatright">
 				<input type="text" name="applicant_fName" id="applicantSearch" placeholder="First Name"></input>
