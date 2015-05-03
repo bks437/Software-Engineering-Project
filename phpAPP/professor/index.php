72793e8ede1e5deb28623d731faf8b7698941303
JET005 <ethanwtsn.watson5@gmail.com>
2015-05-03 18:54:10 -0500

    added to professor page

--- phpAPP/professor/index.php
+++ phpAPP/professor/index.php
@@ -68,7 +68,7 @@
 
 					<?php
 					while( $course = pg_fetch_array($courses, null, PGSQL_ASSOC)){
-						echo "<option value=\"$course[c_id]\">$course[c_id]</option>"
+						echo "<option value=\"$course[c_id]\">$course[c_id]</option>";
 					}
 
 					?>
