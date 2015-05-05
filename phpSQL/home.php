cd8c317c4aeb97e47b85f2ce9dabb1be17e06d8e
JET005 <ethanwtsn.watson5@gmail.com>
2015-05-04 23:50:54 -0500

    lets applicant apply for ta/pla for a certian semester

--- phpSQL/home.php
+++ phpSQL/home.php
@@ -12,8 +12,8 @@
 	if(isset($semester[name])){
 		$_SESSION[Semester]=$semester[name];
 		if(isset($_POST[submit])){
-			pg_prepare($dbconn,"apply",'INSERT INTO applicant_applies_for_semester(username,semester) VALUES ($1,$2)');
-			$apply=pg_execute($dbconn,"apply",array($_SESSION[username],$_SESSION[Semester]));
+			pg_prepare($dbconn,"apply",'INSERT INTO applicant_applies_for_semester(username,semester) VALUES ($1,$2)')or die('error4 ' . pg_last_error());
+			$apply=pg_execute($dbconn,"apply",array($_SESSION[username],$_SESSION[Semester]))or die('error4 ' . pg_last_error());
 			if($apply==false){
 				echo "You failed to apply.";
 			}
