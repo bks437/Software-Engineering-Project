e2192802e0971fe57eca0590dc14414c5ea58dcd
JET005 <ethanwtsn.watson5@gmail.com>
2015-05-05 21:26:02 -0500

    added button for user to update thier courses

--- phpSQL/home.php
+++ phpSQL/home.php
@@ -145,7 +145,7 @@
 					echo "</tr>";
 				}
 				echo "</table>";
-				echo "<br><br><br>";
+				echo "<br><br><br><button onclick=\"window.href.location='../Application/courses.php'\">Update Courses</button>";
 				if(!isset($applied[username])){
 					echo "<form action=\"home.php\" method=\"POST\">";
  					echo "<input type=\"submit\" name=\"submit\" value=\"Click here to apply\">";
